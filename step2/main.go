fb3f8a6fa2656721fa409bda59389e90bb4207a1
Elliot Wright <wright.elliot@gmail.com>
2016-08-07 17:16:19 +0100

    Moved some more things around. Simplified some of the code to keep things shorter for the presentation.

--- step2/main.go
+++ step2/main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
-	"os"
-	"runtime"
+	"flag"
+	"fmt"
+	"net/http"
 
-	"github.com/SeerUK/GopherScore/step2/cmd"
-	"github.com/eidolon/gonsole"
+	"github.com/SeerUK/GopherScore/stepx/handlers"
+	"github.com/SeerUK/GopherScore/stepx/modules/wow"
+	"github.com/gorilla/mux"
 )
 
-func init() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-}
-
 func main() {
-	app := gonsole.NewApplication("SeerUK/GopherScore", "1.0.0")
-	app.AddCommands([]gonsole.Command{
-		cmd.SearchCommand(),
-	})
+	var apiKey string
+	var addr string
+	var port int
 
-	app.Run(os.Args[1:])
+	flag.StringVar(&apiKey, "apikey", "", "An API key for the Battle.net API.")
+	flag.StringVar(&addr, "addr", "0.0.0.0", "An address to bind to.")
+	flag.IntVar(&port, "port", 8080, "A port to bind to.")
+	flag.Parse()
+
+	calculator := wow.AggregateScoreCalculator{}
+	calculator.AddCalculator(wow.AchievementScoreCalculator{})
+	calculator.AddCalculator(wow.ItemsScoreCalculator{})
+	calculator.AddCalculator(wow.ProfessionsScoreCalculator{})
+	calculator.AddCalculator(wow.ProgressionScoreCalculator{})
+
+	client := wow.NewApiClient(apiKey)
+	handler := handlers.NewScoreHandler(&calculator, client).Handler
+
+	router := mux.NewRouter()
+	router.HandleFunc("/score/{region}/{realm}/{name}", handler)
+
+	fmt.Println(fmt.Sprintf("Listening on http://%s:%d/", addr, port))
+
+	http.ListenAndServe(fmt.Sprintf("%s:%d", addr, port), router)
 }
